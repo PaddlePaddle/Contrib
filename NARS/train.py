ec881daffe742232baab143c2b3214ff4b802cc1
sunlizhuang <1114694057@qq.com>
2021-07-05 17:49:25 +0800

    up

--- NARS/train.py
+++ NARS/train.py
@@ -1,3 +1,18 @@
+#encoding=utf8
+# Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import argparse
 import time
 import paddle
@@ -33,6 +48,7 @@ def main(args):
     with paddle.no_grad():
         feats=[]
         for i in range(args.R + 1):
+            #数据集请自行在OGB官网下载，并按照官网教程生产训练集，或者在AiStudio上查询data88697
             feature = np.load(f'../data/data88697/feat{i}.npy')
             feats.append(paddle.to_tensor(feature))
         # feats = preprocess_features(g, rel_subsets, args, device)
